e684b18cc844c6751238a60a52f50385f2f8f402
Santipap Putla <61160023@go.buu.ac.th>
2021-10-10 22:36:34 +0700

    Test web app

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const port = process.env.port || 8080
 const app = express()
+const host = '0.0.0.0';
 
 app.get('/', (req, res) => {
     res.json({message: 'Hello world!'})
 })
 
-app.listen(port, () => {
+app.listen(port,host, () => {
     console.log('The app is up and running!')
 })
 module.exports = app
